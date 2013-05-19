fe201b028b2bcdc3977d32b75ae54c3b498af1b7
Danche Nikoloska <danchenikoloska@gmail.com>
2013-05-19 17:46:52 +0200

    Pravila2

---
+++ TrainYourBrain/Pravila.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrainYourBrain
+{
+    public partial class Pravila : Form
+    {
+        public Pravila()
+        {
+            InitializeComponent();
+        }
+    }
+}
